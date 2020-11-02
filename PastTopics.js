ea563f88b32bae69d8e08eeb17a9d8b70728c014
voromahery <daniel.her@onja.org>
2020-11-02 14:56:41 +0300

    Add a new icon

--- PastTopics.js
+++ PastTopics.js
@@ -6,21 +6,9 @@ export default function PastTopics(props) {
     return (
         <article key={props.topic.id}>
             <button className="delete" id={props.topic.id} onClick={props.deleteHandleClick}>
-                <svg
-                    className="w-6 h-6"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    id={props.topic.id}
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        id={props.topic.id}
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
-                    ></path>
+                <svg class="w-6 h-6" id={props.topic.id} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" id={props.topic.id} stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16">
+                    </path>
                 </svg>
             </button>
             <h5 className="topic-text">{props.topic.title}</h5>
--- dist/react-tea-time-topic.e31bb0bc.js
+++ dist/react-tea-time-topic.e31bb0bc.js
@@ -29939,18 +29939,18 @@ function PastTopics(props) {
     id: props.topic.id,
     onClick: props.deleteHandleClick
   }, /*#__PURE__*/_react.default.createElement("svg", {
-    className: "w-6 h-6",
+    class: "w-6 h-6",
+    id: props.topic.id,
     fill: "none",
     stroke: "currentColor",
     viewBox: "0 0 24 24",
-    id: props.topic.id,
     xmlns: "http://www.w3.org/2000/svg"
   }, /*#__PURE__*/_react.default.createElement("path", {
+    "stroke-linecap": "round",
     id: props.topic.id,
-    strokeLinecap: "round",
-    strokeLinejoin: "round",
-    strokeWidth: "2",
-    d: "M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
+    "stroke-linejoin": "round",
+    "stroke-width": "2",
+    d: "M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
   }))), /*#__PURE__*/_react.default.createElement("h5", {
     className: "topic-text"
   }, props.topic.title), /*#__PURE__*/_react.default.createElement("p", null, "Discussed on ", discussedOnDate.toLocaleDateString()));
